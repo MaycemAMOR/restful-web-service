30883517a652fefaa468741a01b411c5dc44bbc1
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-08 17:58:45 +0200

    - Implementing GET Methods for User Resource

---
+++ src/main/java/com/mytech/rest/webservices/restfulwebservice/user/UserDaoService.java
@@ -0,0 +1,29 @@
+package com.mytech.rest.webservices.restfulwebservice.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    private static final List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+        users.add(new User(4, "kim", LocalDate.now().minusYears(15)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<User> userPredicate = user -> user.getId().equals(id);
+        return users.stream().filter(userPredicate).findFirst().get();
+    }
+}
---
+++ src/main/java/com/mytech/rest/webservices/restfulwebservice/user/UserResource.java
@@ -0,0 +1,30 @@
+package com.mytech.rest.webservices.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+
+    private final UserDaoService userDaoService;
+
+    //dependency Injection in constrictor
+    public UserResource(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return userDaoService.findOne(id);
+    }
+
+}
