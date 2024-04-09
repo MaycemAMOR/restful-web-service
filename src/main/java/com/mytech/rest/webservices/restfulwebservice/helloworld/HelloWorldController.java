692c64e7adf07eadc9ad01bc664d600afaa1a482
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-09 12:48:09 +0200

    Exploring Internationalization for REST API.

--- src/main/java/com/mytech/rest/webservices/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/mytech/rest/webservices/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.mytech.rest.webservices.restfulwebservice.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     @GetMapping("/hello-world")
     public String getHelloWorldString(){
         return "Hello World";
@@ -20,5 +29,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean getHelloWorldPathVariable(@PathVariable String name){
         return  new HelloWorldBean(String.format("Hello World, %s",name ));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String getHelloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null, "Default Message",locale);
+        //return "Hello World";
     }
 }
