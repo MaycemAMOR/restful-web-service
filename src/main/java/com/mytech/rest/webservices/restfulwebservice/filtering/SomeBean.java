9e2314a6607632b9580034ed2627492be4331d48
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-09 15:39:54 +0200

    - Implementing Static Filtering for REST API.

---
+++ src/main/java/com/mytech/rest/webservices/restfulwebservice/filtering/SomeBean.java
@@ -0,0 +1,39 @@
+package com.mytech.rest.webservices.restfulwebservice.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"field1", "field3"}) // Static filtering for the hole class properties
+public class SomeBean {
+
+    private final String field1;
+    //@JsonIgnore // static filtering : de na passer ce field dans le json
+    private final String field2;
+    private final String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    @Override
+    public String toString() {
+        return "SomeBean{" +
+                "field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/mytech/rest/webservices/restfulwebservice/user/User.java
+++ src/main/java/com/mytech/rest/webservices/restfulwebservice/user/User.java
@@ -1,5 +1,6 @@
 package com.mytech.rest.webservices.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -7,9 +8,12 @@ import java.time.LocalDate;
 
 public class User {
     private Integer id;
+
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("User_name")
     private String name;
     @Past(message = "Birth Date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
