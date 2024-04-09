5ce8a443499b4969fb0e11431fe6ff569adb284d
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-09 14:34:22 +0200

    Versioning REST API - Request Param, Header and Content Negotiation:
    Request Parameter versioning-Amazon:
    
    http://localhost:8080/person?version=1
    {
      "name": "Bob Charly"
    }
    
    http://localhost:8080/person?version=2
    {
      "name": {
        "firstName": "Bob",
        "lastName": "Charly"
      }
    }
    
    (Custom) headers versioning-Microsoft :
    
    Same-URL headers=[X-API-VERSION=1]
    http://localhost:8080/person?version=1
    {
      "name": "Bob Charly"
    }
    
    Same-URL headers=[X-API-VERSION=2]
    {
      "name": {
        "firstName": "Bob",
        "lastName": "Charly"
      }
    }
    
    Media type versioning (a.k.a "content negotiation" or "accept header")- GitHub:
    
    Same-URL produces=application/vnd.company.app-V1+json
    {
      "name": "Bob Charly"
    }
    
    Same-URL produces=application/vnd.company.app-V2+json
    {
      "name": {
        "firstName": "Bob",
        "lastName": "Charly"
      }
    }
    
    factors to consider : 1- URI Pollution
    					  2- Misuse Of HTTP headers :Utilisation abusive des en-têtes HTTP
    					  3- Caching
    					  4- Can we execute the request on the browser? no in the case of headers versioning and media type versioning
    					  5-API Documentation so hard in the case of headers versioning and media type versioning
    					  ==> Summary : no Perfect Solution
    
    Recommendations: - think about versioning even before you need it.
    				- Best practices : one Enterprise => one versioning Approach

--- src/main/java/com/mytech/rest/webservices/restfulwebservice/versioning/VersioningPersonController.java
+++ src/main/java/com/mytech/rest/webservices/restfulwebservice/versioning/VersioningPersonController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charly");
@@ -12,6 +13,36 @@ public class VersioningPersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charly"));
+    }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charly");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charly"));
+    }
+
+    @GetMapping(path = "/person", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charly");
+    }
+
+    @GetMapping(path = "/person", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charly"));
+    }
+
+    @GetMapping(path = "/person", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charly");
+    }
+
+    @GetMapping(path = "/person", produces = "application/vnd.company.app-V2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
         return new PersonV2(new Name("Bob", "Charly"));
     }
 }
