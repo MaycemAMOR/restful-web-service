da1071187c448e868f121c87c6779c20cbc61d3e
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-09 02:26:30 +0200

    - Enhancing POST Method to return correct HTTP Status Code and Location.

--- src/main/java/com/mytech/rest/webservices/restfulwebservice/user/UserResource.java
+++ src/main/java/com/mytech/rest/webservices/restfulwebservice/user/UserResource.java
@@ -1,33 +1,48 @@
 package com.mytech.rest.webservices.restfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class UserResource {
 
 
-    private final UserDaoService userDaoService;
+    private final UserDaoService service;
 
 
     //dependency Injection in constrictor
     public UserResource(UserDaoService userDaoService) {
-        this.userDaoService = userDaoService;
+        this.service = userDaoService;
     }
 
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
-        return userDaoService.findAll();
+        return service.findAll();
     }
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return userDaoService.findOne(id);
+        return service.findOne(id);
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        userDaoService.saveUser(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.saveUser(user);
+        // envoyer location aprés la création de chaque user
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build(); /* amiliorer le code status de retour sucess response => 200
+                                                            created status => 201
+                                                            no content => 204
+                                                            unauthorized (when authorization fails) => 401
+                                                            Bad Request (validation error) => 400
+                                                            resourcer is not found => 404
+                                                            server exception => 500*/
     }
 }
